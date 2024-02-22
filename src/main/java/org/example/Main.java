8d4b1f9fa435ca6f7dc2efeb5f96524e8582f9e6
Pravin Nichal <nichal.pravin2001@gmail.com>
2024-02-22 21:57:21 +0530

    commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+
+import org.example.jade.Window;
+
+public class Main {
+    public static void main(String[] args) {
+        Window window = Window.get();
+        window.run();
+    }
+}
---
+++ src/main/java/org/example/jade/Window.java
@@ -0,0 +1,49 @@
+package org.example.jade;
+
+import org.lwjgl.glfw.GLFWErrorCallback;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+    private static Window window = null;
+    private int width, height;
+    private String title;
+    private long glfwWindow;
+
+    private Window() {
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "Mario";
+    }
+
+    public static Window get() {
+        if (Window.window == null) {
+            Window.window = new Window();
+        }
+        return Window.window;
+    }
+
+    public void run() {
+        System.out.println("Hello LWJGL");
+        init();
+        loop();
+    }
+
+    public void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unalle to initialize GLFW");
+        }
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+
+    }
+
+    public void loop() {
+
+    }
+}
